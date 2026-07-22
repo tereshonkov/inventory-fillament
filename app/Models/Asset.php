d9d43b323ad81962b1e06fd11bf72b231c1b6c3d
tereshonkov <tereshonkov.dima@gmail.com>
2026-07-22 15:45:42 +0300

    Fix syntax by adding a comma in the casts array in Asset model

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -6,14 +6,16 @@ use App\Enums\AssetStatus;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 #[Fillable(['name', 'inventory_number', 'serial_number', 'notes', 'year', 'location_id', 'type_id', 'custodian_id', 'holder_id', 'status'])]
 class Asset extends Model
 {
+    // Не забыть исправить нормальное отображение даты через миграцию!
     protected function casts(): array
     {
         return [
-            'status' => AssetStatus::class
+            'status' => AssetStatus::class,
         ];
     }
 
