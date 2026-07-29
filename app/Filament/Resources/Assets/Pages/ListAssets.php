1a3b96e4fb5f34311d1695a9bd8c919a3eae1af9
tereshonkov <tereshonkov.dima@gmail.com>
2026-07-29 12:34:50 +0300

    Add maxContentWidth property to ListAssets for improved layout control

--- app/Filament/Resources/Assets/Pages/ListAssets.php
+++ app/Filament/Resources/Assets/Pages/ListAssets.php
@@ -5,10 +5,13 @@ namespace App\Filament\Resources\Assets\Pages;
 use App\Filament\Resources\Assets\AssetResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\Width;
 
 class ListAssets extends ListRecords
 {
     protected static string $resource = AssetResource::class;
+
+    protected Width|string|null $maxContentWidth = 'full';
 
     protected function getHeaderActions(): array
     {
