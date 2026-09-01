29d2bee0429ae64e9cc8ecb7df92a853d5b82834
tereshonkov <tereshonkov.dima@gmail.com>
2026-09-01 17:05:25 +0300

    Update welcome.blade.php to set flex direction for improved layout alignment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,7 @@
             text-align: center;
             height: 100vh;
             display: flex;
+            flex-direction: column;
             justify-content: center;
             align-items: center;
         }
