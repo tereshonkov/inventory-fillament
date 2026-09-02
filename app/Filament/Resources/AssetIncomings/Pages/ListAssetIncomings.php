333284d6fd71c1c2fdddde678c123979c690502c
tereshonkov <tereshonkov.dima@gmail.com>
2026-09-02 19:49:10 +0300

    Add tabs for incoming asset types in ListAssetIncomings page

--- app/Filament/Resources/AssetIncomings/Pages/ListAssetIncomings.php
+++ app/Filament/Resources/AssetIncomings/Pages/ListAssetIncomings.php
@@ -6,6 +6,9 @@ use App\Filament\Resources\AssetIncomings\AssetIncomingResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Enums\Width;
+use App\Enums\IncomingType;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListAssetIncomings extends ListRecords
 {
@@ -17,6 +20,24 @@ class ListAssetIncomings extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+        public function getTabs(): array
+    {
+        $baseQuery = fn() => static::getResource()::getEloquentQuery();
+
+        return [
+            'all' => Tab::make('Усі')
+                ->badge((clone $baseQuery())->count()),
+
+            'incoming storage' => Tab::make('Отримано на складі')
+                ->badge((clone $baseQuery())->where('incoming_type', IncomingType::NEW)->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('incoming_type', IncomingType::NEW)),
+
+            'incoming department' => Tab::make('Отримано з підрозділу')
+                ->badge((clone $baseQuery())->where('incoming_type', IncomingType::ALREADY_IN_USE)->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('incoming_type', IncomingType::ALREADY_IN_USE)),
         ];
     }
 }
