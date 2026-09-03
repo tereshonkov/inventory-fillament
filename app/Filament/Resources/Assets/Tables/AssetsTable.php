0713b3eff74cc706f577d2b3e8a3c9cee91ceaa3
tereshonkov <tereshonkov.dima@gmail.com>
2026-09-03 14:21:03 +0300

    Fix layout for filters in AssetsTable configuration

--- app/Filament/Resources/Assets/Tables/AssetsTable.php
+++ app/Filament/Resources/Assets/Tables/AssetsTable.php
@@ -112,7 +112,7 @@ class AssetsTable
                     ->options(fn() => Asset::query()->whereNotNull('year')->distinct()->orderByDesc('year')->pluck('year', 'year'))
                     ->searchable()
                     ->multiple(),
-            ])
+            ], layout: FiltersLayout::Modal)
             ->recordActions([
                 // ViewAction::make(),
                 // EditAction::make(),
